8c48a75398b42e07648e4de327b49d891a035e3f
markusFrost <andreywystawkin@yandex.ru>
2015-11-20 01:35:10 +0300

    add replace using regex

--- RegExHelper/RegExHelper/Form1.Designer.cs
+++ RegExHelper/RegExHelper/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.rtbConstants = new System.Windows.Forms.RichTextBox();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.rtbSql = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(881, 49);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // rtbConstants
+            // 
+            this.rtbConstants.Location = new System.Drawing.Point(12, 32);
+            this.rtbConstants.Name = "rtbConstants";
+            this.rtbConstants.Size = new System.Drawing.Size(856, 144);
+            this.rtbConstants.TabIndex = 1;
+            this.rtbConstants.Text = resources.GetString("rtbConstants.Text");
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Location = new System.Drawing.Point(12, 392);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(856, 159);
+            this.rtbResult.TabIndex = 3;
+            this.rtbResult.Text = "";
+            // 
+            // rtbSql
+            // 
+            this.rtbSql.Location = new System.Drawing.Point(12, 214);
+            this.rtbSql.Name = "rtbSql";
+            this.rtbSql.Size = new System.Drawing.Size(856, 127);
+            this.rtbSql.TabIndex = 4;
+            this.rtbSql.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 640);
+            this.Controls.Add(this.rtbSql);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.rtbConstants);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.RichTextBox rtbConstants;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.RichTextBox rtbSql;
     }
 }
 
--- RegExHelper/RegExHelper/Form1.cs
+++ RegExHelper/RegExHelper/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,68 @@ namespace RegExHelper
         {
             InitializeComponent();
         }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<string, string> map = new Dictionary<string, string>();
+
+            string pattern1 = @"\b\w+\s*\=\s*\|\b\w+\|\;";
+            string input = rtbConstants.Text;
+
+            string pattern2 = @"\w+";
+
+            string sqlValue = rtbSql.Text;
+
+            input = input.Replace("\"", "|");
+
+            foreach (Match match in Regex.Matches(input, pattern1, RegexOptions.IgnoreCase))
+            {
+                string key = null;
+                string value = null;
+
+                foreach (Match match1 in Regex.Matches( match.Value, pattern2, RegexOptions.IgnoreCase))
+                {
+                    if (key == null)
+                    {
+                        key = match1.Value;
+                    }
+                    else if ( value == null )
+                    {
+                        value = match1.Value;
+                    }
+
+                }
+
+                if ( !map.ContainsKey( key ) )
+                {
+                    map.Add(key, value);
+                }
+            }
+
+           
+
+            foreach (string key in map.Keys)
+            {
+                string replace = "";
+
+                replace += " \"";
+                replace += " + ";
+                replace += key;
+                replace += " + ";
+                 replace += "\" ";
+
+                 string pat = @"\b" + map[key] + @"\b";
+                Regex rgx = new Regex(pat);
+
+                sqlValue = rgx.Replace(sqlValue, replace);
+            }
+
+            rtbResult.Text = sqlValue;
+        }
+
+        
+
+
     }
 }
