4795bd611d769a44f3e4bc4fce102a232f4afe54
markusFrost <andreywystawkin@yandex.ru>
2015-11-20 11:33:35 +0300

    redesign form2

--- RegExHelper/RegExHelper/Form2.Designer.cs
+++ RegExHelper/RegExHelper/Form2.Designer.cs
@@ -28,19 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbConstantRes = new System.Windows.Forms.RichTextBox();
+            this.rtbQuery = new System.Windows.Forms.RichTextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(408, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Введите sql запрос";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(406, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Результирующие константы";
+            // 
+            // rtbConstantRes
+            // 
+            this.rtbConstantRes.Location = new System.Drawing.Point(8, 302);
+            this.rtbConstantRes.Name = "rtbConstantRes";
+            this.rtbConstantRes.Size = new System.Drawing.Size(856, 201);
+            this.rtbConstantRes.TabIndex = 8;
+            this.rtbConstantRes.Text = "";
+            // 
+            // rtbQuery
+            // 
+            this.rtbQuery.Location = new System.Drawing.Point(8, 32);
+            this.rtbQuery.Name = "rtbQuery";
+            this.rtbQuery.Size = new System.Drawing.Size(856, 231);
+            this.rtbQuery.TabIndex = 7;
+            this.rtbQuery.Text = "";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(893, 321);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(139, 43);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(893, 220);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(139, 43);
+            this.btnGenerate.TabIndex = 11;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.ClientSize = new System.Drawing.Size(1044, 408);
+            this.ClientSize = new System.Drawing.Size(1044, 528);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbConstantRes);
+            this.Controls.Add(this.rtbQuery);
             this.Name = "Form2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbConstantRes;
+        private System.Windows.Forms.RichTextBox rtbQuery;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnGenerate;
     }
 }
--- RegExHelper/RegExHelper/Form2.cs
+++ RegExHelper/RegExHelper/Form2.cs
@@ -16,5 +16,10 @@ namespace RegExHelper
         {
             InitializeComponent();
         }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
