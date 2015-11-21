7183bc51d7bcd9bfacfd2d177c465fae704fba4f
markusFrost <andreywystawkin@yandex.ru>
2015-11-21 11:43:00 +0300

    full realisation of mechanism save last class name

---
+++ RegExHelper/RegExHelper/MainDbHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegExHelper
+{
+    public class MainDbHelper : DbContext
+    {
+        public DbSet<ResultItemWork> ResultItemWorks { get; set; }
+        public DbSet<ItemSavedData> ItemsSavedData { get; set; }
+    }
+}
